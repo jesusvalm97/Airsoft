2b37f203e06f458abe3a720f411010ce5da1b544
jesusvalm97 <52518344+jesusvalm97@users.noreply.github.com>
2020-09-30 08:16:30 -0500

    Clase app y modificacion del codigo del form

---
+++ AirsoftDeliciasChihuahua/AirsoftDeliciasChihuahua.Standard/App.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirsoftDeliciasChihuahua.Standard
+{
+    public class App : OKHOSTING.UI.App
+    {
+        protected override void StartController(OKHOSTING.UI.Controller controller)
+        {
+            base.StartController(controller);
+
+            //OKHOSTING.UI.CSS.Style style = new OKHOSTING.UI.CSS.Style();
+            //var assembly = typeof(App).Assembly;
+            //var resourceName = "RealWellness.UI.Style.css";
+
+            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            //using (StreamReader reader = new StreamReader(stream))
+            //{
+            //    string css = reader.ReadToEnd();
+            //    style.Parse(css);
+            //}
+
+
+
+        }
+    }
--- AirsoftDeliciasChihuahua/AirsoftDeliciasChihuahua/Form1.cs
+++ AirsoftDeliciasChihuahua/AirsoftDeliciasChihuahua/Form1.cs
@@ -10,11 +10,20 @@ using System.Windows.Forms;
 
 namespace AirsoftDeliciasChihuahua
 {
-    public partial class Form1 : Form
+    public partial class Form1 : OKHOSTING.UI.Net4.WinForms.Page
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            var config = new AirsoftDeliciasChihuahua.Standard.Config();
+            config.Start();
+
+            App = new AirsoftDeliciasChihuahua.Standard.App();
+            App.MainPage = this;
         }
     }
 }
